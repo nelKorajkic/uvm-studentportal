d06e24ed4826b5db6cdc7c5d1188325747fca60b
abeard1 <AlexBeard@ip0af51c2d.int.uvm.edu>
2017-04-01 10:51:17 -0400

    added form

--- uploadFile.php
+++ uploadFile.php
@@ -1,44 +1,11 @@
-<?php require("includes/top.php"); ?>
-<?php require("includes/nav.php"); ?>
+<?php require("includes/top.php");
+      require("includes/nav.php"); ?>
 
-<body id = 'login'>
-<div class="container content-container">
-	<div class="row">
-		<div class="col-sm-12"  style="margin-top: 30px">
-			<div class="col-sm-offset-3 col-sm-6">
-				<div class="panel panel-default">
-					<!-- HEADER -->
-					<div class="panel-heading">
-						<h4 class="text-center">Welcome to NoteTaker</h4>
-					</div>
+<br>
 
-					<!-- BODY -->
-					<div class="panel-body">
-						<form class="form-horizontal" action="" mehtod="post">
-							<div class="form-group">
-								<label class="control-label col-sm-3">Net ID</label>
-								<div class="col-sm-9">
-									<input type="text" class="form-control" placeholder="Please enter your NetID....." name="netID" value="tnguye20">
-								</div>
-							</div>
+<form action="viewFile.php" method="POST" enctype="multipart/form-data">
+          <textarea name="txt" rows="28" cols="150" onfocus="this.select()"></textarea>
+         <input type="submit"/>
+</form>
 
-							<div class="form-group">
-								<label class="control-label col-sm-3">Password</label>
-								<div class="col-sm-9">
-									<input type="password" class="form-control" placeholder="Please enter your password....." name="password" value="aaaaaa">
-								</div>
-							</div>
-						</form>
-					</div>
-
-					<!-- FOOTER -->
-					<div class="panel-footer">
-						<p class="text-right"><input type="submit" class="btn btn-success" value="Log In" name="logIn"></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-</body>
 <?php require("includes/footer.php"); ?>

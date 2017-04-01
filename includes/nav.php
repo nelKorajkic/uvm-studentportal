cbae4574c7536df2408b0ca55b02f2c280eb1ae9
CuongLai <Laic5000@gmail.com>
2017-04-01 09:39:15 -0400

    Added nav.php

---
+++ includes/nav.php
@@ -0,0 +1,17 @@
+<nav id="nav">
+  <button type="button" class="btn btn-default" aria-label="Left Align">
+    <span aria-hidden="true">Logo</span>
+  </button>
+
+  <button type="button" class="btn btn-default" aria-label="Left Align">
+    <span aria-hidden="true">Upload</span>
+  </button>
+
+  <button type="button" class="btn btn-default">
+    <span aria-hidden="true">Become a Notaker</span>
+  </button>
+
+  <button type="button" class="btn btn-default">
+    <span aria-hidden="true">Login</span>
+  </button>
+</nav>

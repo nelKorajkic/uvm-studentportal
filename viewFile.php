00f2f47f9ae0b64c2fca870f1e53aab0e0cfe049
abeard1 <AlexBeard@ip0af51c2d.int.uvm.edu>
2017-04-01 11:30:27 -0400

    watson translate demo

--- viewFile.php
+++ viewFile.php
@@ -3,10 +3,17 @@
 require("includes/top.php");
 require("includes/nav.php");
 
+$sourcelanguage = "en";
+$targetLanguage = "es";
+
 if(isset($_POST['txt'])){
        $userInput = $_POST['txt'];
-       $userInputPlus = str_replace(' ', '+', $userInput);
-       echo $userInputPlus;
+       //$userInputPlus = str_replace(' ', '+', $userInput);
+       //echo $userInput;
+       echo "original: " .$userInput;
+       echo "watson: ".watsonLanguageTranslate ($sourcelanguage,$targetLanguage,$userInput);
+       //json_decode($userInput);
+       //echo "end";
        
 }
 

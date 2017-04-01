49e56c51ef27f78d799819902c2abbf5e2845bf0
Emma <ertait@uvm.edu>
2017-04-01 10:31:05 -0400

    watson services for language translator

---
+++ watson/watsonServices.php
@@ -0,0 +1,23 @@
+<?php
+
+//This traslates text from source languge to target langage. words in text must be seprated by "+".
+function watsonLanguageTranslate ($sourcelanguage,$targetLanguage,$text){
+$curl = curl_init();
+	$username = 'e42e3020-6ab2-4db6-862b-08283b7d8ebf';
+    $password ='Ek4cpXsiNsk0';
+    $headers = array('Authorization: Basic '. base64_encode($username.':'.$password));
+	curl_setopt_array($curl, array(
+		CURLOPT_HTTPHEADER=>$headers,
+	    CURLOPT_RETURNTRANSFER => 1,
+	    CURLOPT_URL =>"https://gateway.watsonplatform.net/language-translator/api/v2/translate?source=".$sourcelanguage."&target=".$targetLanguage."&text=".$text,
+	    CURLOPT_USERAGENT => 'Codular Sample cURL Request',
+	    CURLOPT_VERBOSE=>TRUE,
+	    CURLOPT_SSL_VERIFYPEER=>FALSE,
+));
+$resp = curl_exec($curl);
+curl_close($curl);
+echo $resp;
+}
+
+
+?>

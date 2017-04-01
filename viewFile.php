5f15af91ddbdfef5db256f7a227c257b7779428f
abeard1 <AlexBeard@ip0af51c2d.int.uvm.edu>
2017-04-01 11:31:27 -0400

    Revert "watson translate demo"
    This reverts commit 00f2f47f9ae0b64c2fca870f1e53aab0e0cfe049.

--- viewFile.php
+++ viewFile.php
@@ -3,17 +3,10 @@
 require("includes/top.php");
 require("includes/nav.php");
 
-$sourcelanguage = "en";
-$targetLanguage = "es";
-
 if(isset($_POST['txt'])){
        $userInput = $_POST['txt'];
-       //$userInputPlus = str_replace(' ', '+', $userInput);
-       //echo $userInput;
-       echo "original: " .$userInput;
-       echo "watson: ".watsonLanguageTranslate ($sourcelanguage,$targetLanguage,$userInput);
-       //json_decode($userInput);
-       //echo "end";
+       $userInputPlus = str_replace(' ', '+', $userInput);
+       echo $userInputPlus;
        
 }
 

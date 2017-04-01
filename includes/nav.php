f95653b6e4863e44c87c46862d6904203a0ea6ef
Anel.Korajkic <anel.korajkic@uvm.edu>
2017-04-01 14:06:59 -0400

    Set up logo links

--- includes/nav.php
+++ includes/nav.php
@@ -1,15 +1,19 @@
 <nav class="nav">
-  <a class="btn btn-default" aria-label="Left Align">
-    <img src="images/smallLogo.png"></img>
-  </a>
+
 
 	<?php if (!isset($_SESSION['user'])): ?>
+		<a class="btn btn-default" id="logo" aria-label="Left Align" href="#">
+			<img src="images/smallLogo.png"></img>
+		</a>
   	<a type="button" id="floatRight" class="btn btn-default" href="login.php">
     	<span aria-hidden="true">Login</span>
   	</a>
 		<?php endif; ?>
 
 		<?php if (isset($_SESSION['user'])): ?>
+			<a class="btn btn-default" id="logo" aria-label="Left Align" href="hub.php">
+		    <img src="images/smallLogo.png"></img>
+		  </a>
 		<a type="button" id="floatRight" class="btn btn-default" href="logout.php">
     	<span aria-hidden="true">Logout</span>
   	</a>

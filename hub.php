69b367e196185e0306f63bcf980cd36e3300c3ce
CuongLai <Laic5000@gmail.com>
2017-04-01 11:43:37 -0400

    fixed hub

--- hub.php
+++ hub.php
@@ -2,16 +2,17 @@
 include 'includes/top.php';
 include 'includes/nav.php';
 
+
 print '<table>';
-if ($result->num_rows > 0)
-{
-  while($row = $result->fetch_assoc())
-  {
-      print '<tr>';
-      print '<td>' . $row["fldName"] . '</td>';
-      print '</td></tr>';
-    }
-  }
+// if ($result->num_rows > 0)
+// {
+//   while($row = $result->fetch_assoc())
+//   {
+//       print '<tr>';
+//       print '<td>' . $row["fldName"] . '</td>';
+//       print '</td></tr>';
+//     }
+//   }
 
 print '</table>';
 
